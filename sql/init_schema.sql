baedcab598eb06e908ac36ed2d17f4bd80d5d15f
LeshaSid <lesha.sidoruk@gmail.com>
2026-06-24 21:15:41 +0300

    feat: designed the data schema

---
+++ sql/init_schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE events (
+    id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    timestamp_event timestamptz,
+    user_id bigint,
+    anonymous_id VARCHAR(100),
+    event_name VARCHAR(50),
+    device_type VARCHAR(50),
+    traffic_source VARCHAR(50),
+    properties JSONB
+);
+
+CREATE INDEX idx_events_event_name_timestamp_event ON events(event_name, timestamp_event);
+CREATE INDEX idx_events_user_id ON events(user_id);
+CREATE INDEX idx_events_properties ON events(properties) USING GIN;
+
+CREATE TABLE ab_exposures (
+    user_id bigint,
+    timestamp_ab timestamptz,
+    experiment_name VARCHAR(100),
+    group_ab CHAR(1),
+    PRIMARY KEY (user_id,experiment_name)
+);
